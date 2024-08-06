12330942c3f885b9591e70404a99aa710f3424b7
rushikeshmj <rushikeshja@cybage.com>
2024-08-06 11:19:07 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -22,3 +22,38 @@ variable "db_password" {
   type        = string
   default     = "password123"
 }
+
+variable "frontend_desired_capacity" {
+  description = "Desired number of frontend instances"
+  type        = number
+  default     = 2
+}
+
+variable "backend_desired_capacity" {
+  description = "Desired number of backend instances"
+  type        = number
+  default     = 2
+}
+
+variable "key_name" {
+  description = "Key name for SSH access"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+  default     = "10.0.2.0/24"
+}
