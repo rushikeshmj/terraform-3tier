8ab806db2338e8b9abe8f144efde08e1d6b2e7ba
rushikeshmj <rushikeshja@cybage.com>
2024-08-06 11:04:27 +0530

    Create main.tf

---
+++ database/main.tf
@@ -0,0 +1,34 @@
+# database/main.tf
+resource "aws_security_group" "database_sg" {
+  name = "database-sg"
+  
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    security_groups = [aws_security_group.backend_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_instance" "example" {
+  identifier        = "example-db"
+  engine            = "postgres"
+  instance_class    = "db.t2.micro"
+  name              = "exampledb"
+  username          = var.db_username
+  password          = var.db_password
+  allocated_storage = 20
+
+  vpc_security_group_ids = [aws_security_group.database_sg.id]
+}
+
+output "endpoint" {
+  value = aws_db_instance.example.endpoint
+}
