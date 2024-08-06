8a1b561e5efe12c0e92323461f5c89c92e6a5747
rushikeshmj <rushikeshja@cybage.com>
2024-08-06 11:18:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,14 +3,24 @@ provider "aws" {
   region = "us-east-1"
 }
 
+module "vpc" {
+  source = "./vpc"
+}
+
 module "frontend" {
   source = "./frontend"
+  vpc_id = module.vpc.vpc_id
+  public_subnet_id = module.vpc.public_subnet_id
+  load_balancer_dns = module.frontend.load_balancer_dns
 }
 
 module "backend" {
   source = "./backend"
+  vpc_id = module.vpc.vpc_id
+  private_subnet_id = module.vpc.private_subnet_id
 }
 
 module "database" {
   source = "./database"
+  vpc_id = module.vpc.vpc_id
 }
